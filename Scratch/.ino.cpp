24c1a212077613ebfdf7cb4180db674c855d53ac
fitzpatricksrus <junk@fitzpatircksr.us>
2014-10-03 19:05:48 -0700

    Removed a Release directory from the repository.  They're generated binaries.

--- Scratch/.ino.cpp
+++ Scratch/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2014-10-02 00:15:20
+//This file has been generated on 2014-10-02 00:26:50
 
 #include "Arduino.h"
 #include <Arduino.h>
--- Scratch/spec.d
+++ Scratch/spec.d
@@ -1,2 +1,2 @@
 spec.o:  \
- /Users/Dad/GoogleDrive/arduino/WorkspaceGit/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
+ /Users/Dad/GoogleDrive/arduino/Pinduino/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
