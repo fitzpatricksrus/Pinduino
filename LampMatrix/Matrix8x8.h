c300efca15b5dbec15776c260ae38f8e1436f37a
fitzpatricksrus <junk@fitzpatircksr.us>
2014-10-04 17:24:56 -0700

    Thought these had been added, but I was wrong.  Adding now.

---
+++ LampMatrix/Matrix8x8.h
@@ -0,0 +1,19 @@
+/*
+ * Matrix8x8.h
+ *
+ *  Created on: Oct 4, 2014
+ *      Author: Dad
+ */
+
+#ifndef MATRIX8X8_H_
+#define MATRIX8X8_H_
+
+#include "MatrixPattern.h"
+
+class Matrix8by8 {
+public:
+	static MatrixPattern* getPattern(int ndx);
+};
+
+
+#endif /* MATRIX8X8_H_ */
