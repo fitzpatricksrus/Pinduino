cfce02b74f10c28a65afa6d7ef7f4ee5b488e811
fitzpatricksrus <junk@fitzpatircksr.us>
2014-10-13 23:40:40 -0700

    Initial checkin

---
+++ LampMatrix/scheduler/Scheduler.cpp
@@ -0,0 +1,70 @@
+/*
+ * Scheduler.cpp
+ *
+ *  Created on: Oct 13, 2014
+ *      Author: Dad
+ */
+
+#include "Scheduler.h"
+
+namespace SchedulerLib {
+
+Scheduler::Scheduler(){
+	action[0].action = NULL;
+	action[0].delay = 65535;
+	action[0].time = 0;
+	currentSize = 0;
+}
+
+//Check to see if it's time to call any of the functions
+void Scheduler::update(){
+	//flush/update all
+	for (byte i=0; i<currentSize; i++){
+		//it is time to call the function associated with action[i]
+		if ((millis() - action[i].time) > action[i].delay){
+			action[i].action(); //call function
+			//this was the last action in queue
+			if (i==0 && currentSize==1){ currentSize=0; }
+			//this is the second action of two possible, only one left
+			else if (i==1 && currentSize==2){ currentSize=1; }
+			//the last action has executed, cut it off
+			else if (i+1==currentSize){
+				currentSize--;
+			}
+			//all other elements
+			else if (i+1<currentSize){
+				action[i] = action[i+1];
+				defragment(); //get rid of the two copies in memory
+			}
+		}
+	}
+}
+
+//Schedule a functioncall in 'time' milliseconds.
+void Scheduler::schedule(function userAction, unsigned int time){
+	if (currentSize<NUMBER_OF_SCHEDULED_ACTIONS){
+		action[currentSize].action = userAction;
+		action[currentSize].delay = time;
+		action[currentSize].time = millis();
+		currentSize++;
+	}
+}
+
+//Removes all redundant actions from memory
+void Scheduler::defragment(){
+	byte defrag=0;
+	for (byte i=0; i<currentSize; i++){
+		if(i+1 < currentSize && action[i] == action[i+1]){
+			defrag=i+1;
+			while (defrag+1 < currentSize){
+				action[defrag] = action[defrag+1];
+				defrag++;
+			}
+			currentSize--;
+		}
+	}
+}
+
+}
+
+
---
+++ LampMatrix/scheduler/Scheduler.h
@@ -0,0 +1,53 @@
+/*
+ * Scheduler.h
+ *
+ *  Created on: Oct 13, 2014
+ *      Author: Dad
+ */
+
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+
+
+#include <Arduino.h>
+//#include <WProgram.h>
+
+namespace SchedulerLib {
+
+//provide a typedef for a void function pointer
+typedef void (*function)();
+
+typedef struct schedule_action_s {
+	function action;
+	unsigned long time;
+	unsigned int delay;
+	//assignment operator
+	schedule_action_s& operator=(const schedule_action_s& rhs){
+		action = rhs.action;
+		time = rhs.time;
+		delay = rhs.delay;
+		return *this;
+	}
+	//boolean equality
+	bool operator==(const schedule_action_s& rhs) const{
+		return (action == rhs.action && time == rhs.time && delay == rhs.delay);
+	}
+}schedule_action;
+
+
+class Scheduler {
+	public:
+		Scheduler();
+		void update();
+		void schedule(function userAction, unsigned int time);
+	private:
+		static const byte NUMBER_OF_SCHEDULED_ACTIONS = 10;
+		void defragment();
+		schedule_action action[NUMBER_OF_SCHEDULED_ACTIONS];
+		byte currentSize;
+};
+
+
+} /* namespace SchedulerLib */
+
+#endif /* SCHEDULER_H_ */
