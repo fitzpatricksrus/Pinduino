13fae8a94a87af79caf5eff1d46cae680c119e62
John Fitzpatrick <jfitzpatrick@jfitzpatrick-macbookair2.roam.corp.google.com>
2014-10-17 20:11:53 -0700

    updating timers interface to be more friendly

--- LampMatrix/scheduler/Timer.h
+++ LampMatrix/scheduler/Timer.h
@@ -19,8 +19,28 @@ public:
 		virtual void setup() = 0;
 		virtual void run() = 0;
 	};
-	enum Prescalar { PS1 = (1 << CS10), PS8 = (1 << CS11), PS64 = (1 << CS10) | (1 << CS11),
+	enum PrescalarX { PS1 = (1 << CS10), PS8 = (1 << CS11), PS64 = (1 << CS10) | (1 << CS11),
 		PS256 = (1 << CS12), PS1024 = (1 << CS10) | (1 << CS12) };
+	enum TCCR1AMode = { 
+		OFF_MASK = ~((1 << COM1A0) | (1 << COM1A1) | (1 << PWM10) | (1 << PWM11)),
+		COMPARE_TOGGLE = (1 << COM1A0), 
+		COMPARE_CLEAR = (1 << COM1A1), 
+		COMPARE_SET = (1 << COM1A0) | (1 << COM1A1),
+		PWM8BIT = (1 << PWM10),
+		PWM9BIT = (1 << PWM11),
+		PWM10BIT = (1 << PWM10) | (1 << PWM11),	
+		STOP_TIMER_MASK = ~((1 << CS10) | (1 << CS11) | (1 << CS12)),
+	};
+	enum TCCR1BMode = {
+		INPUT_CAPTURE_NOISE_CANCELING_ON = (1 << ICNC1),
+		IMPUT_CAPTURE_ON_RISING_EDGE = (1 << ICES1),
+		CLEAR_TIMER_ON_MATCH = (1 << CTC1),
+		NO_PRESCALAR = (1 << CS10),
+		PRESCALAR_8 = (1 << CS11),
+		PRESCALAR_64 = (1 << CS10) ,
+		PRESCALAR_256 = (1 << CS11),
+		PRESCALAR_1024 = (1 << CS11),
+	};
 
 	virtual ~Timer();
 	Timer::Callback* getCallback() const;
@@ -29,7 +49,13 @@ public:
 	virtual void setPrescalar(Prescalar scalar) = 0;
 	virtual int getTicks() const = 0;
 	virtual void setTicks(int ticks) = 0;
+	
+	enum Prescalar { TIMER_OFF, PS1, PS8, PS64, PS256, PS1024, 
+		PSExternalFalling, PSExternalRising };
+	virtual void setPrescalar(Prescalar p) = 0;
 
+	
+	
 	static Timer& TIMER1;
 
 
