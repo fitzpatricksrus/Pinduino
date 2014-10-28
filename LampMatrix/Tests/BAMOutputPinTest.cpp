ab156b4326ed7c2394bd5d7e0c4fac2fdbbd6bc1
fitzpatricksrus <junk@fitzpatircksr.us>
2014-10-28 00:56:41 -0700

    dont know

--- LampMatrix/Tests/BAMOutputPinTest.cpp
+++ LampMatrix/Tests/BAMOutputPinTest.cpp
@@ -52,7 +52,7 @@ void BAMOutputPinTest::loop() {
 		count = (count + 16) & 0x1FF;
 		for (int i = 0; i < 8; i++) {
 			int val = count;
-#if 1
+#if 0
 			val = val & 0x1FF;
 			val = (val < 256) ? val : (511 - val);
 #else
