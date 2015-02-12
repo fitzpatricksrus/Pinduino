a25a2f02a98a8cd88c537ebc5fc72cb29a6e5989
fitzpatricksrus <junk@fitzpatircksr.us>
2015-02-11 23:49:47 -0800

    Initial checkin

---
+++ WPCMITM/WPCMITM.ino
@@ -0,0 +1,37 @@
+//The setup function is called once at startup of the sketch
+
+enum {
+	SW_COL_1 = 1,
+	SW_ROW_2 = 2,
+	SW_ROW_1 = 5,
+	LMP_ROW = 7,
+	LMP_COL = 8,
+	SOL_2 = 9,
+	SOL_4 = 10,
+	SOL_3 = 11,
+	SOL_1 = 12,
+	TRIAC = 13,
+	D7 = 15,
+	D6 = 17,
+	D5 = 19,
+	D4 = 21,
+	D3 = 23,
+	D2 = 25,
+	D1 = 27,
+	D0 = 29,
+	BLANK = 31,
+	SW_JMP_1 = 32,
+	SW_JPM_2 = 33,
+	ZERO_CROSS = 34
+} WPC_J111;
+
+void setup()
+{
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
