5544f8ef2cd2fef1022e3e4d7587a2ad4af1912d
fitzpatricksrus <junk@fitzpatircksr.us>
2015-02-25 23:19:06 -0800

    experiments

--- WPCController/WPCController.ino
+++ WPCController/WPCController.ino
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include <PinChangeInt.h>
 
+//#define JUST_ROW_COL_SIGNAL
 #define DIRECT_DATA_PASSTHROUGH
-#define BUFFERED_DATA_PASSTHROUGH
+//#define DIRECT_DATA_PASSTHROUGH2
+//#define BUFFERED_DATA_PASSTHROUGH
 
 // PORTC
 static const byte ROW_OUT = 30;
@@ -12,48 +14,65 @@ static const byte SOL1_OUT = 33;
 static const byte SOL2_OUT = 34;
 static const byte SOL3_OUT = 35;
 static const byte SOL4_OUT = 36;
-static const byte UNUSED = 37;
+static const byte EN_ARDUINO_OUT = 37;
 
-//
+// control bus
 static const byte CLK_DATA_OUT = 38;	//PD7
 static const byte EN_DATA_OUT = 39;	//PG2
 static const byte CLK_DATA_IN = 40;	//PG1
 static const byte EN_DATA_IN = 41;	//PG0
 
-// INPUT
-static const byte ROW_SEL_IN = 22;
-static const byte COL_SEL_IN = 23;
+// input identifier pulses
+static const byte ROW_SEL_IN = A10;
+static volatile bool rowSelectCount = 0;
+static const byte COL_SEL_IN = A15;
+static volatile bool colSelectCount = 0;
 
-static inline void toggle(pin) { digitalWrite(pin, LOW); digitalWrite(pin, HIGH); }
 
+typedef enum DataBusMaster { MPU, MICRO_CONTROLLER } DataBusMaster;
 
-enum class { MPU, ARDUINO } DataBusMaster;
+static int dummy = 0;
 
-static void setDataBusMaster(DataBusMaster master) {
-	if (master == DataBusMaster::MPU) {
+static void delay() {
+	for (int i = 0; i < 7; i++) {
+		dummy += i;
+	}
+}
+
+static inline void toggle(byte pin) {
+	digitalWrite(pin, LOW);
+//	delay();
+	digitalWrite(pin, HIGH);
+}
+
+static inline void setDataBusMaster(byte master) {
+	if (master == MPU) {
 		// arduino pins are in read mode
 		DDRL = 0;
 		// input latch output to data bus
+		digitalWrite(EN_ARDUINO_OUT, HIGH);
 		digitalWrite(EN_DATA_IN, LOW);
 	} else {
-		// input latch in high impedence
+		// input latch in high impedance
 		digitalWrite(EN_DATA_IN, HIGH);
+		digitalWrite(EN_ARDUINO_OUT, LOW);
 		// arduino pins are in write mode
 		DDRL = -1;
 	}
+	delay();
 }
 
 static inline byte readDataBus() {
 	// make sure we're set to read the data bus
-	setDataBusMaster(DataBusMaster::MPU);
+	setDataBusMaster(MPU);
 	
 	// read all of port L
 	return PINL;
 }
 
-static inline void writeDataBus(byte value) 
+static inline void writeDataBus(byte value) {
 	// make sure everything is set to let controller write data bus
-	setDataBusMaster(DataBusMaster::ARDUINO);
+	setDataBusMaster(MICRO_CONTROLLER);
 	
 	// write the entire data bus port
 	PORTL = value;
@@ -69,34 +88,90 @@ static inline void latchExport() {
 	toggle(CLK_DATA_OUT);
 }
 
-static inline signalExport(byte pin) {
+static inline void signalExport(byte pin) {
 	toggle(pin);
 }
+
+#ifdef JUST_ROW_COL_SIGNAL
+static void handleRowInterrupt() {
+	// signal to external client that row data is in output latch
+	signalExport(ROW_OUT);
+}
+
+static void handleColInterrupt() {
+	signalExport(COL_OUT);
+}
+#endif
+
 
 #ifdef DIRECT_DATA_PASSTHROUGH
 static void handleRowInterrupt() {
 	// move data onto data bus from input latch
-	latchImport();
+//	latchImport();
+	toggle(CLK_DATA_IN);
 	
 	// ensure MPU input is on the data bus
-	setDataBusMaster(DataBusMaster::MPU);
+//	setDataBusMaster(MPU);
+
+	// clock data from data bus into output latch
+//	latchExport();
+	toggle(CLK_DATA_OUT);
+
+	// signal to external client that row data is in output latch
+//	signalExport(ROW_OUT);
+	toggle(ROW_OUT);
+}
+
+static void handleColInterrupt() {
+	//	latchImport();
+		toggle(CLK_DATA_IN);
+
+		// ensure MPU input is on the data bus
+	//	setDataBusMaster(MPU);
+
+		// clock data from data bus into output latch
+	//	latchExport();
+		toggle(CLK_DATA_OUT);
+
+		// signal to external client that row data is in output latch
+	//	signalExport(ROW_OUT);
+		toggle(COL_OUT);
+}
+#endif
+
+#ifdef DIRECT_DATA_PASSTHROUGH2
+
+static void handleInterrupt() {
+	// move data onto data bus from input latch
+	latchImport();
+
+	// ensure MPU input is on the data bus
+	setDataBusMaster(MPU);
+
+	// read data on data bus
+	byte data = readDataBus();
+
+	// take control of bus
+	setDataBusMaster(MICRO_CONTROLLER);
+
+	// write the data just read
+	writeDataBus(data);
 
 	// clock data from data bus into output latch
 	latchExport();
+
+	// signal to external client that column data is in output latch
+}
+
+static void handleRowInterrupt() {
+	handleInterrupt();
 
 	// signal to external client that row data is in output latch
 	signalExport(ROW_OUT);
 }
 
 static void handleColInterrupt() {
-	// move data onto data bus from input latch
-	latchImport();
-	
-	// ensure MPU input is on the data bus
-	setDataBusMaster(DataBusMaster::MPU);
-
-	// clock data from data bus into output latch
-	latchExport();
+	handleInterrupt();
 
 	// signal to external client that column data is in output latch
 	signalExport(COL_OUT);
@@ -114,24 +189,23 @@ comes high.
 */
 
 
-static byte currentColumn;		// the column that is currently on
 static byte pendingRows;		// the current row values
 static byte matrixLamps[8];
 
 static void handleRowInterrupt() {
 	// move data onto data bus from input latch
 	latchImport();
-	setBusMaster(DataBusMaster::MPU);
+	setDataBusMaster(MPU);
 	// stash the row of data somewhere until a column is turned on.
 	// this works because rows are only updated when all columns
 	// are off on WPC.
-	pendingRow = readDataBus();
+	pendingRows = readDataBus();
 }
 
 static void handleColInterrupt() {
 	// move data onto data bus from input latch
 	latchImport();
-	setBusMaster(DataBusMaster::MPU);
+	setDataBusMaster(MPU);
 	byte column = readDataBus();
 	for (byte i = 0; i < 8; i++) {
 		if (column & 1) {
@@ -143,8 +217,6 @@ static void handleColInterrupt() {
 	}
 }
 
-
-
 #endif
 
 void setup() {
@@ -153,13 +225,16 @@ void setup() {
 		digitalWrite(i, HIGH);
 		pinMode(i, OUTPUT);
 	}
+	for (byte i = 42; i <= 49; i++) {
+		pinMode(i, INPUT);
+	}
 
 	// set trigger pins to input
 	pinMode(ROW_SEL_IN, INPUT);
 	pinMode(COL_SEL_IN, INPUT);
 
 	// allow input data latch to write to data bus
-	setDataBusMaster(DataBusMaster::MPU);
+	setDataBusMaster(MPU);
 
 	//TODO you should write to the output latch to clear crud before you enable it.
 	// allow output latch to write to output lines
@@ -167,7 +242,17 @@ void setup() {
 
 	attachPinChangeInterrupt(ROW_SEL_IN,handleRowInterrupt,RISING);
 	attachPinChangeInterrupt(COL_SEL_IN,handleColInterrupt,RISING);
+
+	Serial.begin(57600);
 }
 
 void loop() {
+	if (rowSelectCount) {
+		Serial.println("row");
+		rowSelectCount = false;
+	}
+	if (colSelectCount) {
+		Serial.println("col");
+		colSelectCount = false;
+	}
 }
