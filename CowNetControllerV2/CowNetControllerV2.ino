f7e99f544977545699cdd523cf7a481f7babaa5c
fitzpatricksrus <junk@fitzpatircksr.us>
2015-03-17 23:59:08 -0700

    Initial checkin

---
+++ CowNetControllerV2/CowNetControllerV2.ino
@@ -0,0 +1,38 @@
+
+/*
+ * The hardware looks as follows:
+ * 8 pins, 0 - 7, used for data bus I/O.  Since it's shared for input and output,
+ * pin 8 is used to select the direction: LOW input, HIGH output.
+ *
+ * There are 7 I/O buses that the data signals can be send down: TRIAC, LAMP_COL,
+ * LAMP_ROW, SOL1, SOL2, SOL3, SOL4.  These buses are selected through a 74XX138
+ * 3 -> 8 decoder, pins 9 - 11.  This allows fully selectable input through 12 pins.
+ *
+ * One additional pin is used to sense the ZERO_CROSS signal (120Hz) in order to sync
+ * TRIAC changes.  Pin 12.  This signal is read-only.
+ *
+ * Output is over the same 7 I/O buses, 0 - 7.  The output buses are selected through
+ * the another 3 - 8 decoder through the same pins, 9 - 11, but pin 8 should signal
+ * output instead of input.
+ */
+
+
+class CowNetHardware {
+public:
+	CowNetHardware();
+	virtual ~CowNetHardware();
+
+	static CowNetHardware& INSTANCE;
+};
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
